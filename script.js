e5b818272b8ca651921c7aea323593792ef50cc7
amriyas07 <112183814+amriyas07@users.noreply.github.com>
2024-01-30 20:24:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function add() {
+    let a = document.getElementById('p1');
+    let b = document.getElementById('p2');
+    let result = a+b;
+    document.getElementById('res').innerHTML=result;
+    
+}
